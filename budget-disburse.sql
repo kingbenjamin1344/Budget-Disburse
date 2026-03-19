ed0f6baee92c7dd4ae9a4d319372218806958a9a
NessajCK <kingbenjamintheancient@gmail.com>
2026-03-19 15:27:59 +0800

    save for stable version

--- budget-disburse.sql
+++ budget-disburse.sql
@@ -14,15 +14,102 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+
+-- Dumping database structure for budget_disburse
+DROP DATABASE IF EXISTS `budget_disburse`;
+CREATE DATABASE IF NOT EXISTS `budget_disburse` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `budget_disburse`;
+
+-- Dumping structure for table budget_disburse.budget
+DROP TABLE IF EXISTS `budget`;
+CREATE TABLE IF NOT EXISTS `budget` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `officeId` int NOT NULL,
+  `officeName` varchar(191) NOT NULL,
+  `ps` double NOT NULL,
+  `mooe` double NOT NULL,
+  `co` double NOT NULL,
+  `total` double NOT NULL,
+  `dateCreated` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  KEY `Budget_officeId_fkey` (`officeId`),
+  CONSTRAINT `Budget_officeId_fkey` FOREIGN KEY (`officeId`) REFERENCES `office` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table budget_disburse.disbursement
+DROP TABLE IF EXISTS `disbursement`;
+CREATE TABLE IF NOT EXISTS `disbursement` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dvNo` varchar(191) NOT NULL,
+  `payee` varchar(191) NOT NULL,
+  `officeId` int NOT NULL,
+  `officeName` varchar(191) NOT NULL,
+  `expenseType` varchar(191) NOT NULL,
+  `expenseCategory` varchar(191) NOT NULL,
+  `amount` double NOT NULL,
+  `dateCreated` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  KEY `Disbursement_officeId_fkey` (`officeId`),
+  CONSTRAINT `Disbursement_officeId_fkey` FOREIGN KEY (`officeId`) REFERENCES `office` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=20002 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
+-- Dumping structure for table budget_disburse.expense
+DROP TABLE IF EXISTS `expense`;
+CREATE TABLE IF NOT EXISTS `expense` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(191) NOT NULL,
+  `category` varchar(191) NOT NULL,
+  `dateCreated` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- Data exporting was unselected.
+
+-- Dumping structure for table budget_disburse.log
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `message` varchar(191) NOT NULL,
+  `type` varchar(191) NOT NULL,
+  `action` varchar(191) NOT NULL,
+  `performedBy` varchar(191) DEFAULT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  KEY `log_type_createdAt_idx` (`type`,`createdAt`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table budget_disburse.office
+DROP TABLE IF EXISTS `office`;
+CREATE TABLE IF NOT EXISTS `office` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) NOT NULL,
+  `dateCreated` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Office_name_key` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table budget_disburse.useradmin
+DROP TABLE IF EXISTS `useradmin`;
+CREATE TABLE IF NOT EXISTS `useradmin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `useradmin_username_key` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
