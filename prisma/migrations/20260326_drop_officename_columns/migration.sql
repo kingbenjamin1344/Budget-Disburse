a747a2b2c4ca773827aa77a5cadc1f578ff8d602
NessajCK <kingbenjamintheancient@gmail.com>
2026-03-26 22:33:24 +0800

    Remove broken DROP COLUMN migration, use only MODIFY to make columns nullable

--- prisma/migrations/20260326_drop_officename_columns/migration.sql
+++
@@ -1,4 +0,0 @@
--- NOTE: This migration failed in production and will be rolled back
--- Use migration 20260326_fix_officename_columns instead
-ALTER TABLE `budget` DROP COLUMN IF EXISTS `officeName`;
-ALTER TABLE `disbursement` DROP COLUMN IF EXISTS `officeName`;
