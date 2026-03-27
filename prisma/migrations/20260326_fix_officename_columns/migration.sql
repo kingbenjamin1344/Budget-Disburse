cdfbb3826bd4ba84f72c5d81c8ff38c6fa9ef8eb
NessajCK <kingbenjamintheancient@gmail.com>
2026-03-27 11:41:16 +0800

    Fix: Remove broken migration causing FK constraint violation

--- prisma/migrations/20260326_fix_officename_columns/migration.sql
+++
@@ -1,5 +0,0 @@
--- Fix: Make officeName nullable first, then drop later
--- This handles the NULL constraint violation
-
-ALTER TABLE `budget` MODIFY `officeName` VARCHAR(191) NULL;
-ALTER TABLE `disbursement` MODIFY `officeName` VARCHAR(191) NULL;
